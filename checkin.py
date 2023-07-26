48964c4096f7cdd14f753dacd65f58465c21198c
刘振龙 <dragonliu@buaa.edu.cn>
2023-07-26 10:38:32 +0800

    fix selenium bug

--- checkin.py
+++ checkin.py
@@ -1,16 +1,19 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from time import sleep
 
 def signin():
     # 设置Chrome浏览器选项
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')  # 无界面模式，不弹出浏览器窗口
+    service = Service(executable_path=r'./chromedriver_linux64/chromedriver')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
     # 创建Chrome浏览器实例
-    browser = webdriver.Chrome('./chromedriver_linux64/chromedriver', options=chrome_options)
+    browser = webdriver.Chrome(service=service, options=options)
 
     # 登陆
     browser.implicitly_wait(20) # 如果找不到元素，每隔半秒钟再去界面上查看一次， 直到找到该元素， 或者过了20秒最大时长。
